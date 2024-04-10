d3669787523faffb1bfbcc8c8bd7777ccdd0610a
jjhelguero <joshua.helguero@mercari.com>
2024-04-10 16:05:53 -0500

    rename var

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec')
 
 async function run() {
     // Get some input values
-    const batches = core.getInput('n', {required: true})
+    const batches = core.getInput('batches', {required: true})
 
     const matrix = await exec.exec(`./build-matrix.js ${batches}`)
     
